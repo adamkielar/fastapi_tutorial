661f1388608efcbd5953413d16391d7f25bb6a5f
Adam <adam@adamkielar.pl>
2020-10-22 14:45:28 +0200

    Add API endpoint items and models

--- app/api/main.py
+++ app/api/main.py
@@ -1,10 +1,34 @@
+from enum import Enum
 from typing import Optional
 
 from fastapi import FastAPI
+
+
+class ModelName(str, Enum):
+    ALEXNET = 'alexnet'
+    RESNET = 'resnet'
+    LENET = 'lenet'
+
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
+async def root():
     return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.ALEXNET:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
